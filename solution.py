283913a1467c83c7e723e1a2520843fab92abc73
Greg Liuzzi <gregliuzzi@Gregs-iMac.home>
2021-09-07 17:32:10 -0400

    fixed bugs

--- solution.py
+++ solution.py
@@ -5,8 +5,7 @@
 def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "In Slack, what is the secret passphrase posted in the \
-        #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
+    if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
@@ -20,11 +19,9 @@ def welcome_assignment_answers(question):
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - \
-    The answer should be a numeric number":
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 5
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - \
-     The answer should be a numeric number":
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
 
     return(answer)
