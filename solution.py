a010fd2b16c860b88541ca706040921c956a6220
Greg Liuzzi <gregliuzzi@Gregs-iMac.home>
2021-09-07 17:27:18 -0400

    fixed some bugs

--- solution.py
+++ solution.py
@@ -26,8 +26,6 @@ def welcome_assignment_answers(question):
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - \
      The answer should be a numeric number":
         answer = 4
-    else:
-        return "That is not a valid question."
 
     return(answer)
 # Complete all the questions.
